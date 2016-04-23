37984da94781865b80c76de8059027595fd301a3
Fredrik Strandin <strandin72@gmail.com>
2016-04-23 18:11:26 +0200

    Add support for serialtion for Claim

---
+++ src/Core.MongoDB/Serializer/ClaimProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization;
+
+namespace IdentityServer4.Core.MongoDB.Serializer
+{
+    public class ClaimProvider : IBsonSerializationProvider
+    {
+        public IBsonSerializer GetSerializer(Type type)
+        {
+            if (type == typeof(Claim))
+            {
+                return new ClaimSerializer();
+            }
+
+            return null;
+        }
+    }
+}
