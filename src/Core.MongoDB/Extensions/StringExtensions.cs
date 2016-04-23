05461250c534bb872b1fe68b75b21c8888e437a3
Fredrik Strandin <strandin72@gmail.com>
2016-04-23 16:12:33 +0200

    Add support for ICorsPolicyService

---
+++ src/Core.MongoDB/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    //https://www.youtube.com/watch?v=AU-4oLUV5VU&noredirect=1
+
+    public static class StringExtensions
+    {
+        public static string GetOrigin(this string url)
+        {
+            if (url != null && (url.StartsWith("http://") || url.StartsWith("https://")))
+            {
+                var idx = url.IndexOf("//", StringComparison.Ordinal);
+                if (idx > 0)
+                {
+                    idx = url.IndexOf("/", idx + 2, StringComparison.Ordinal);
+                    if (idx >= 0)
+                    {
+                        url = url.Substring(0, idx);
+                    }
+                    return url;
+                }
+            }
+
+            return null;
+        }
+    }
+}
