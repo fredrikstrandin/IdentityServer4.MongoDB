d15b70bee410a3916461ff8e6500bd6b9a176587
Fredrik Strandin <strandin72@gmail.com>
2016-04-23 16:30:03 +0200

    Smail change to comments

--- src/Core.MongoDB/Services/MongoDB/MongoDBProfileService.cs
+++ src/Core.MongoDB/Services/MongoDB/MongoDBProfileService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+﻿// Copyright (c) Fredrik Strandin. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 using IdentityModel;
@@ -17,7 +17,7 @@ using IdentityServer4.MongoDB.Models;
 namespace IdentityServer4.Core.Services.MongoDB
 {
     /// <summary>
-    /// In-memory user service
+    /// MongoDB user service
     /// </summary>
     public class MongoDBProfileService : IProfileService
     {
